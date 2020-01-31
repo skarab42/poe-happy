23186a16bcef37d13fa79edaefc58e60b7773857
Sébastien Mischler (aka skarab) <lautr3k@users.noreply.github.com>
2020-01-31 17:36:01 +0100

    i18n

--- app/js/main/poe/getLeagues.js
+++ app/js/main/poe/getLeagues.js
@@ -1,12 +1,17 @@
 const axios = require("axios");
 const error = require("../error");
+const store = require("../store");
 
 async function getLeagues() {
   // https://www.pathofexile.com/developer/docs/api-resource-leagues#list
   // http://api.pathofexile.com/leagues?type=main&realm=pc&compact=1
   // https://www.pathofexile.com/api/trade/data/leagues
   try {
-    const url = "https://www.pathofexile.com/api/trade/data/leagues";
+    let lang = store.get("language", "en");
+    if (lang === "en") {
+      lang = "www";
+    }
+    const url = `https://${lang}.pathofexile.com/api/trade/data/leagues`;
     const response = await axios.get(url);
     return response.data.result;
   } catch (e) {
